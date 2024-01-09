160ccfcb9393b33af53514bd5e07c6046ec4e7c3
rohantandale1107 <rohanhtandale11@gmail.com>
2024-01-09 17:09:38 +0530

    01 lecture

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountID=14453;
+let accountEmail="rohna@gmail.com"
+var accountPass="12345"
+accountCity="Pune"
+let accountState
+
+accountEmail="rt@gmail.com"
+accountPass="121212"
+accountCity="mumbai"
+
+
+/*
+    Dont use var
+    because of issue in block scope and  functional scope
+*/
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPass,accountCity,accountState])
