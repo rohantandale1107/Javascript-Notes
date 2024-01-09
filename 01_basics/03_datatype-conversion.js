ebff7069a753bc7ca6400d3113d282701277eff8
rohantandale1107 <rohanhtandale11@gmail.com>
2024-01-09 17:44:58 +0530

    datatype conversions

---
+++ 01_basics/03_datatype-conversion.js
@@ -0,0 +1,29 @@
+let score="33"
+
+console.log(typeof score);
+
+
+let value= Number (score)
+console.log(typeof value);
+ 
+console.log(value);
+
+// "33"=> 33
+// "33abc"=> NaN(not a value)
+// true=1, false=0
+// null => 0
+//undefined=> undefined
+
+let isLoggedin=1
+
+let BoolisLoggedin= Boolean (isLoggedin);
+console.log(BoolisLoggedin);
+
+//1=> true  0=> false
+// ""=> false    (empty string)
+//"rohan"=> true
+
+let Num= 33
+let stringNum= String(Num)
+console.log(stringNum);
+console.log(typeof stringNum);
