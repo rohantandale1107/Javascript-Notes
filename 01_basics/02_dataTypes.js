28bb2e50d0eafb7585c6098ed26d1e405ab970ce
rohantandale1107 <rohanhtandale11@gmail.com>
2024-01-09 17:22:09 +0530

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,1 +1,20 @@
-console.log(2+2)
+console.log("rohan")
+
+let name="rohan"
+let age=20
+let isLoggedin=false
+
+// Data types
+//number=> 2 power of 52
+// bigint
+// string=> ""
+// boolean => true/false
+//null=> standalone value (represntaion empty value)
+//        let temp= null;
+// undefined=> when value is not assigned
+//        let state 
+
+
+console.log(typeof name); //string]
+console.log(typeof undefined); // => undefined;
+console.log(typeof null) // => object
