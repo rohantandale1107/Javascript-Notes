e0e197fbba15e22d70a12ec0ba634d89bda1497b
rohantandale1107 <rohanhtandale11@gmail.com>
2024-01-12 16:32:10 +0530

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+//=> true or false
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+//
+
+console.log("2" > 1);
+console.log( 1 > "2"); //string converts for comparison
+
+//
+console.log(null>0);
+console.log(null == 0);
+console.log(null <= 0);
+console.log(null >= 0);
+
+console.log(undefined >= 0);
+console.log(undefined >= 0);
+console.log(undefined >= 0);
+
+// avoid this type of writing 
+// give priority to clean code
+//note: equality check and comparisons are not same
+
+/// ===
+
+console.log("2" === 2);
