57d4dc3532a7c364ec1597c06a4f387b158d71df
rohantandale1107 <rohanhtandale11@gmail.com>
2024-02-04 22:42:24 +0530

    object

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -22,7 +22,7 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "c", 4: "d"}
 
 const obj3 = { obj1, obj2}
-const obj4 = Object.assign({}, obj1, obj2)  
+const obj4 = Object.assign({}, obj1, obj2) 
 // first parenthesis position ({}) is target, and other are source that gonna stored in 1st object (1st positioned)
 
 
