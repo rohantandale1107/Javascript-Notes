9d6b4e8a91c32420442036d1782f9ee9609fea30
rohantandale1107 <rohanhtandale11@gmail.com>
2025-03-19 06:08:39 +0530

    commit

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,11 +2,15 @@ const name="rohan"
 console.log("Hello, my name is");
 console.log(name);
 
+const accountID= 1212;
+const accountEmail="rohan@gmail.com"
+const accountPass="pass@123"
+let accountCity="pune"
+let accountState="maharashtra"
 
 /*
     Dont use var
     because of issue in block scope and  functional scope
 */
-
-// console.log(accountID);
-// console.table([accountID,accountEmail,accountPass,accountCity,accountState])
+console.log(accountID);
+console.table([accountID,accountEmail,accountPass,accountCity,accountState])
